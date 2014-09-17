bdc9bf45bce86e07fa5be1915585d3aa9b47f222
simon <cloud9209@gmail.com>
2014-09-17 17:29:35 +0900

    사인업 페이지에 인증 뷰를 추가함. 여기에서 '인증번호 받기' 버튼을 누를때 함수와 실제 인증을 하는 '인증'버튼을 누를때 함수를 signup.java에 넣어둠. 우선은 토스트로 메세지를 띄워주는거로만 이벤트 설정을 해놓음.

--- android/BobMain/src/com/appspot/wecookbob/SignUp.java
+++ android/BobMain/src/com/appspot/wecookbob/SignUp.java
@@ -12,8 +12,8 @@ import android.widget.*;
 
 
 public class SignUp extends Activity {
-	Button btnLogIn, btnSignUp;
-	ViewFlipper vf;
+	Button btnLogIn, btnSignUp, btnSignUpComplete;
+	ViewFlipper vf_login_signup, vf_signup_phonecheck;
 	Animation slide_in_left, slide_out_right;
 
 	@Override
@@ -24,18 +24,27 @@ public class SignUp extends Activity {
 	 
 		btnLogIn = (Button) findViewById(R.id.btn_login);
 		btnSignUp = (Button) findViewById(R.id.btn_signup);
-		vf = (ViewFlipper) findViewById(R.id.viewflipper);
+		btnSignUpComplete = (Button) findViewById(R.id.btn_signup_complete);
+		
+		vf_login_signup = (ViewFlipper) findViewById(R.id.vf_login_signup);
+		vf_signup_phonecheck = (ViewFlipper) findViewById(R.id.vf_signup_phonecheck);
 	 
         slide_in_left = AnimationUtils.loadAnimation(this,
                 android.R.anim.slide_in_left);
         slide_out_right = AnimationUtils.loadAnimation(this,
                 android.R.anim.slide_out_right);
  
-        vf.setInAnimation(slide_in_left);
-        vf.setOutAnimation(slide_out_right);
+        vf_login_signup.setInAnimation(slide_in_left);
+        vf_login_signup.setOutAnimation(slide_out_right);
  
+        vf_signup_phonecheck.setInAnimation(slide_in_left);
+        vf_signup_phonecheck.setOutAnimation(slide_out_right);
+ 
+        
 		btnLogIn.setOnClickListener(btnClickListener);
         btnSignUp.setOnClickListener(btnClickListener);
+        btnSignUpComplete.setOnClickListener(btnClickListener);
+
     }
 	
 		
@@ -63,9 +72,9 @@ public class SignUp extends Activity {
 	        case R.id.btn_login:
 	        	if(v==findViewById(R.id.btn_login))
 	        	{
-	        		vf.setDisplayedChild(vf.indexOfChild(findViewById(R.id.vf_login)));
+	        		vf_login_signup.setDisplayedChild(vf_login_signup.indexOfChild(findViewById(R.id.vf_login)));
 	        	}else{
-	        		vf.stopFlipping();
+	        		vf_login_signup.stopFlipping();
 	        	}
 	        		
 	            break;
@@ -73,21 +82,39 @@ public class SignUp extends Activity {
 	        	
 	        	if(v==findViewById(R.id.btn_signup))
 	        	{
-	        		vf.setDisplayedChild(vf.indexOfChild(findViewById(R.id.vf_signup)));
+	        		vf_login_signup.setDisplayedChild(vf_login_signup.indexOfChild(findViewById(R.id.vf_signup)));
 	        	}else{
-	        		vf.stopFlipping();
+	        		vf_login_signup.stopFlipping();
 	        	}
 	            break;
+	        
+	            
+	        case R.id.btn_signup_complete:
+	        	if(v==findViewById(R.id.btn_signup_complete))
+	        	{
+	        		vf_signup_phonecheck.setDisplayedChild(vf_signup_phonecheck.indexOfChild(findViewById(R.id.vf_certification)));
+	        	}else{
+	        		vf_signup_phonecheck.stopFlipping();
+	        	}
 	        default:
-	            break;
+	        break;
+
 	        }
 	         
 	    }
 	};
 	
-    public void signUp(View view) {
-    	Toast.makeText(SignUp.this, "가입완료되었습니다",
+	public void get_certificate_number(View view) {
+    	Toast.makeText(SignUp.this, "문자로 받은 인증번호를 입력하십시오",
                 Toast.LENGTH_SHORT).show();
+    }
+
+	
+	public void certificate_phonenumber(View view) {
+    	Toast.makeText(SignUp.this, "인증이 완료되었습니다 혹은 다시 입력해주세요",
+                Toast.LENGTH_SHORT).show();
+    	
+    	showBobMain(view);
     }
 	
 	
