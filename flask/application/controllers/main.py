05dd6cc7e9b0003cf17bd60c9990898772b6d8de
Dae Hong Kim <grizzlyraccoon@gmail.com>
2014-09-26 14:28:41 +0900

    clean

--- flask/application/controllers/main.py
+++ flask/application/controllers/main.py
@@ -2,9 +2,11 @@ from application import app
 from flask import request
 from application.models import user_manager
 import json
-import urllib2
-from application.models.schema import User
+from gcm import GCM
 
+@app.route('/', methods = ['GET', 'POST'])
+def main():
+	return 'Hi this is BOB'
 
 @app.route('/check-user-id', methods = ['POST'])
 def check_user_id():
@@ -33,11 +35,16 @@ def contacts():
 
 @app.route('/bob', methods = ['GET', 'POST'])
 def bob():
-	data = json.loads(request.form)
-	url = 'http://android.googleapis.com/gcm/send'
-	headers = {'Authorization':'key=AIzaSyDjqsvPD4dJCV4HnEtFY0ay6n4EmGGekQ0', 'Content-Type':'application/json'}
-	registration_id = user_manager.get_registration_ids(data['user_id'])
-	return None
+	bob = GCM(str('AIzaSyBJMaMzLiYxA8eo_THwLjNQP3fvw7CXVqI'))
+	data = {'sender_user_id':'azulpanda'}
+	reg_ids = ['APA91bF2LHOyoGFif9srXkou_ClojBerqEnlGJk893bLb-96iv_opuHSpUIaAAnCBSJr2nmkOKYLBZ0rceBOw1MLZLje4E7RasXTPl9GidnBi5053sE4ta40as_HbtkqEPrTSkIgzqrS86SNJdPEfXL6T2PH44O9Rw']
+	response = bob.json_request(registration_ids = reg_ids, data = data)
+
+	# data = json.loads(request.form)
+	# url = 'http://android.googleapis.com/gcm/send'
+	# headers = {'Authorization':'key=AIzaSyDjqsvPD4dJCV4HnEtFY0ay6n4EmGGekQ0', 'Content-Type':'application/json'}
+	# registration_id = user_manager.get_registration_ids(data['user_id'])
+	return 'Success'
 
 @app.route('/register', methods = ['POST'])
 def register():
